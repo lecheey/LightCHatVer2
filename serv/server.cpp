10826da54624e2ae69d86fff263b107360a80400
lecheey <154467500+lecheey@users.noreply.github.com>
2024-07-13 19:54:17 +0000

    new features

--- serv/server.cpp
+++ serv/server.cpp
@@ -10,6 +10,7 @@
 #include <filesystem>
 #include <set>
 #include "server.h"
+#include "mysql/mysql.h"
 
 #define CODE_LENGTH 3
 #define COMMAND_LENGTH 6
@@ -24,7 +25,6 @@
 #define QUIT "000"
 
 #define MESSAGE_LENGTH 1024
-#define PORT 7777
 
 int sockd, bind_stat, connection_stat, connection;
 struct sockaddr_in serveraddress, clientaddress;
@@ -32,115 +32,65 @@ socklen_t length;
 char message[MESSAGE_LENGTH];
 int bytes_read;
 
+MYSQL mysql;
+MYSQL_RES* res;
+MYSQL_ROW row;
+
 namespace fs = std::filesystem; 
 
 void Server::serverRuntime(){
 	std::cout << "Запуск сервера..." << std::endl;
 	serverStart();
-
-	while(true){
-        fd_set readset;
-        FD_ZERO(&readset);
-        FD_SET(sockd, &readset);
-
-        for(std::set<int>::iterator it = clients.begin(); it != clients.end(); it++)
-            FD_SET(*it, &readset);
-
-		//std::cout << "i1" << std::endl;
-
-        timeval timeout;
-        timeout.tv_sec = 180;
-        timeout.tv_usec = 0;
-
-        int mx = std::max(sockd, *max_element(clients.begin(), clients.end()));
-        if(select(mx+1, &readset, NULL, NULL, &timeout) <= 0){
-            std::cout << ERROR_MESSAGE << ": Selecting error" << std::endl;
-            exit(3);
-        }
-		
-		std::cout << "m" << std::endl;
-
-        if(FD_ISSET(sockd, &readset)){
-            int connection = accept(sockd, NULL, NULL);
-			if(connection == -1){
-				std::cout << ERROR_MESSAGE << ": Socket is unable to reach client!" << std::endl;
-				exit(1);
-			}	
-			else{
-				std::cout << "Пользователь подключен!" << std::endl;
-			}
-			
-			std::cout << "a" << std::endl;
-            
-            fcntl(connection, F_SETFL, O_NONBLOCK);
-
-            clients.insert(connection);
-        }
-        
-		for(std::set<int>::iterator it = clients.begin(); it != clients.end(); it++){
-		
-			//std::cout << "i2" << std::endl;
-			
-            if(FD_ISSET(*it, &readset)){
-				std::cout << "Начало" << std::endl;
-		std::cout << *it << std::endl;
-
-				if(_switcher < 1){
-				bzero(message, sizeof(message));
-				recv(*it, message, MESSAGE_LENGTH, 0);
-				
-				if(strncmp(message, QUIT, CODE_LENGTH) == 0){
-					std::cout << "Клиент отключился!" << std::endl;
-					std::cout << "Завершение работы сервера...!" << std::endl;
-					exit(1);
-				}
-					
-				std::cout << "середина" << std::endl;
-
-
-				if(strncmp(message, "REGUSR", COMMAND_LENGTH) == 0){
-						userReg(*it, message);
-					}
-				else if(strncmp(message, "LOG_IN", COMMAND_LENGTH) == 0){
-						userAuth(*it, message);
-					}
-				else if(strncmp(message, "LOGOUT", COMMAND_LENGTH) == 0){
-						userLogout(*it, message);
-					}
-				else if(strncmp(message, "GETLST", COMMAND_LENGTH) == 0){
-						sendUserList(*it, message);
-					}
-				else if(strncmp(message, "OPTUSR", COMMAND_LENGTH) == 0){
-						optDialog(*it, message);
-						bzero(message, sizeof(message));
-					}
-				else if(strncmp(message, "HNDSHK", COMMAND_LENGTH) == 0){	
-						++_switcher;	
-						if(_switcher == 1){
-						send(*it, message, sizeof(message), 0);
-						bzero(message, sizeof(message));
-						}
-					}
-				else if(strncmp(message, "USRTYP", COMMAND_LENGTH) == 0){
-						//send(*it, message, sizeof(message), 0);
-					}
-				else{
-					close(sockd);
-					std::cout << "Завершение работы" << std::endl;
-					exit(1);
-				}
-				}
-				else{
-				std::cout << "you r here" << std::endl;
-				send(*it, message, sizeof(message), 0);
 	
-				bzero(message, sizeof(message));
-				recv(*it, message, MESSAGE_LENGTH, 0);
-				}
-			}
+	length = sizeof(clientaddress);
+    connection = accept(sockd,(struct sockaddr*)&clientaddress, &length);
+	if(connection == -1){
+		std::cout << ERROR_MESSAGE << ": Socket is unable to reach client!" << std::endl;
+		exit(1);
+	}	
+	else{
+		std::cout << "Пользователь подключен!" << std::endl;
+	}
+           
+	while(true){
+		bzero(message, sizeof(message));
+		recv(connection, message, MESSAGE_LENGTH, 0);
+				
+		if(strncmp(message, QUIT, CODE_LENGTH) == 0){
+			std::cout << "Клиент отключился!" << std::endl;
+			std::cout << "Завершение работы сервера...!" << std::endl;
+			exit(1);
 		}
+
+		if(strncmp(message, "REGUSR", COMMAND_LENGTH) == 0){
+				userReg(connection, message);
+			}
+		else if(strncmp(message, "LOG_IN", COMMAND_LENGTH) == 0){
+				userAuth(connection, message);
+			}
+		else if(strncmp(message, "LOGOUT", COMMAND_LENGTH) == 0){
+				userLogout(connection, message);
+			}
+		else if(strncmp(message, "GETLST", COMMAND_LENGTH) == 0){
+				sendUserList(connection, message);
+			}
+		else if(strncmp(message, "GETMSG", COMMAND_LENGTH) == 0){
+				sendMsgList(connection, message);
+			}
+		else if(strncmp(message, "OPTUSR", COMMAND_LENGTH) == 0){
+				optDialog(connection, message);
+			}
+		else if(strncmp(message, "USRTYP", COMMAND_LENGTH) == 0){
+				userTyping(connection, message);
+			}
+		else{
+				close(sockd);
+				std::cout << "Завершение работы" << std::endl;
+				exit(1);
+			}
 	}
 	close(sockd);
+	mysql_close(&mysql);
 	std::cout << "Завершение работы" << std::endl;
 	exit(1);
 }
@@ -151,9 +101,7 @@ void Server::serverStart(){
 		std::cout << ERROR_MESSAGE << ": Socket creation falied!" << std::endl;
         exit(1);
     }
-    
-    fcntl(sockd, F_SETFL, O_NONBLOCK);
-    
+     
 	serveraddress.sin_addr.s_addr = INADDR_ANY;
 	serveraddress.sin_port = htons(PORT);
 	serveraddress.sin_family = AF_INET;
@@ -171,9 +119,34 @@ void Server::serverStart(){
 	else{
 		std::cout << "Listening for connections..." << std::endl;
 	}
-    
-	//std::set<int> clients;
-    clients.clear();
+}
+
+void Server::sqlConn(){
+	std::cout << "Подключение к базе данных...";
+	mysql_init(&mysql);
+	if (&mysql == nullptr) {
+		std::cout << ERROR_MESSAGE << " \nНевозможно создать MySQL-дескриптор" << std::endl;
+	}
+ 
+	if (!mysql_real_connect(&mysql, "localhost", sql_user.data(), sql_pass.data(), sql_db.data(), 0, NULL, 0)) {
+		std::cout << ERROR_MESSAGE << "\nНевозможно подключиться к базе данных " << mysql_error(&mysql) << std::endl;
+	}
+	else {
+		std::cout << "успешно" << std::endl;
+	}
+ 
+	mysql_set_character_set(&mysql, "utf8");
+	std::cout << "connection characterset: " << mysql_character_set_name(&mysql) << std::endl;
+	
+	mysql_query(&mysql, "CREATE TABLE userdata(id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, u_surname VARCHAR(255), u_name VARCHAR(255) NOT NULL, u_email VARCHAR(100))");
+	
+	mysql_query(&mysql, "CREATE TABLE userhash(id INT AUTO_INCREMENT NOT NULL PRIMARY KEY references userdata(id), u_pass VARCHAR(255) NOT NULL)");
+	
+	mysql_query(&mysql, "CREATE TABLE msgdata(id_sender INT reverences userdata(id), id_receiver INT reverences userdata(id), msg_date TIMESTAMP, msg_status INT, id_msg INT AUTO_INCREMENT NOT NULL PRIMARY KEY, msg TEXT)");
+
+	mysql_query(&mysql, "DELIMITER | CREATE PROCEDURE add_id() BEGIN INSERT INTO userhash(id, u_pass) VALUES (new.id, null); END; | DELIMITER ;}");
+
+	mysql_query(&mysql, "CREATE DEFINER=`lightchat`@`localhost` TRIGGER add_id AFTER INSERT ON userdata FOR EACH ROW BEGIN INSERT INTO userhash(id, u_pass) VALUES (new.id, null); END }");
 }
 
 void Server::userReg(int index, std::string m){
@@ -193,7 +166,7 @@ void Server::userReg(int index, std::string m){
 		std::cout << ERROR_MESSAGE << std::endl;
 		break;
 	case 5:
-		send(index, ERROR_SYMBOLS, CODE_LENGTH, 0); // ошибка: стоп-символы
+		send(index, ERROR_SYMBOLS, CODE_LENGTH, 0); // ошибка: стоп-символ
 		std::cout << ERROR_MESSAGE << std::endl;
 		break;
 	default:
@@ -203,26 +176,38 @@ void Server::userReg(int index, std::string m){
 }				
 
 void Server::userAuth(int index, std::string m){
-	std::cout << "Вход в систему" << std::endl;
+	std::cout << "Вход в систему...";
 	
-	std::string login_temp, pass_temp;
-	splitReq(m, login_temp, pass_temp);
-
+	std::string login_temp, pass_temp, _q;
+	splitReq(m, login_temp, pass_temp); // извлечение введенных логина и пароля из запроса 
+	
 	int ind = findLogin(login_temp);
 	if(index == -3){
 		send(index, ERROR_NOTFOUND, CODE_LENGTH, 0);
-		std::cout << ERROR_MESSAGE << std::endl;
+		std::cout << ERROR_MESSAGE << "\nпользователь не найден" << std::endl;
 	}
 	else{
-		if(_users[ind]._pass == pass_temp){
+		_q = "SELECT u_pass FROM userhash WHERE id =" + _users[ind]._uid;
+		mysql_query(&mysql, _q.c_str());
+		res = mysql_store_result(&mysql);
+		row = mysql_fetch_row(res);
+		if(row[0] == pass_temp){	
+		//if(_users[ind]._pass == pass_temp){ // если пароль был извлечен в массив пользователей 
 			send(index, SUCCESS, CODE_LENGTH, 0);
 			_users[ind].switchStatus();
 			_users[ind].switchStatusAuth();
+
+			recv(connection, message, MESSAGE_LENGTH, 0);
+			
+			bzero(message, sizeof(message));
+			strcpy(message, (_users[ind]._uid).data());
+			send(index, message, sizeof(message), 0);
+
 			std::cout << "Успешно" << std::endl;	
 		}
 		else{
 			send(index, ERROR_AUTH, CODE_LENGTH, 0);
-			std::cout << ERROR_MESSAGE << std::endl;	
+			std::cout << ERROR_MESSAGE << "\nневерный пароль" << std::endl;	
 		}
 	}
 }
@@ -249,52 +234,84 @@ void Server::userLogout(int index, std::string m){
 			std::cout << ERROR_MESSAGE << std::endl;	
 		}
 	}
-
 }
 
 void Server::userAdd(std::string &l, std::string &p){
 	std::cout << "Создание нового аккаунта...";
+	std::string _q;
 
-	User newUser(l, p);
-	_users.push_back(newUser);
-	++_usercount;
-	
-	writeUser();
+	_q = "INSERT INTO userdata(u_name) values('" + l + "')";
+	mysql_query(&mysql, _q.c_str());
+
+	_q = "UPDATE userhash SET u_pass = '" + p + "' WHERE userhash.id IN ( SELECT id FROM userdata WHERE u_name = '" + l + "')";
+	mysql_query(&mysql, _q.c_str());
 
 	std::cout << "успешно" << std::endl;
-}
-/*
-void Server::handShake(int index){
-	if(swtchr = true){
-		std::string msg = "пользователь зашел в чат";
-		std::string msgPkg = makeReq("USRTYP", msg, getTime());
-		std::cout << msgPkg << std::endl;
-		strcpy(message, msgPkg.data());
-		send(index, message, sizeof(message), 0);
-		swtchr = false;
+	
+	std::string _n, _id, _p;
+	_q = "SELECT userdata.id, u_name, u_pass FROM userhash join userdata on userhash.id = userdata.id WHERE u_name = '" + l + "'";
+	mysql_query(&mysql, _q.c_str());
+	if (res = mysql_store_result(&mysql)) {
+		while (row = mysql_fetch_row(res)) {
+			_id = row[0];
+			_n = row[1];
+			//_p = row[2];
+
+			//User newUser(_id, _n, _p);
+			User newUser(_id, _n);
+			_users.push_back(newUser);
+			_usercount++;
+		}
 	}
-	else{
-		swtchr = true;
-	}
 }
-*/
+
 void Server::sendUserList(int index, std::string m){
 	std::cout << "Передача списка пользователей...";
 	std::string login_temp, dummy;
 	splitReq(m, login_temp, dummy);
-	//std::cout << login_temp << std::endl;
 	
 	if(is_authorized(login_temp)){
-		std::string pkg = pkgUserList();
-		strcpy(message, pkg.data());
-		if(sizeof(message) <= MESSAGE_LENGTH){
+		for(int i = 0; i < _usercount; i++){
+			std::string pkg = makeReq("USTLST", _users[i]._uid, _users[i]._name);
+
+			bzero(message, sizeof(message));
+			strcpy(message, pkg.data());
 			send(index, message, sizeof(message), 0);
-			std::cout << "успешно" << std::endl;
+				
+			recv(connection, message, MESSAGE_LENGTH, 0);
 		}
-		else{
-			send(index, ERROR_NOTFOUND, CODE_LENGTH, 0);
-			std::cout << ERROR_MESSAGE << std::endl;
+		send(index, END, CODE_LENGTH, 0);
+		std::cout << "успешно" << std::endl;
+	}
+	else{
+		errorAccess(index);
+	}	
+}
+void Server::sendMsgList(int index, std::string m){
+	std::cout << "Передача списка сообщений...";
+	std::string from_temp, to_temp;
+	splitReq(m, from_temp, to_temp);
+	
+	std::string _s, _r, _t, _m, _q; 
+	_q = "SELECT id_sender, id_receiver, msg_date, msg From msgdata WHERE (id_sender =" + from_temp + " and id_receiver = " + to_temp + ") or (id_sender = " + to_temp + " and id_receiver = " + from_temp + ") ORDER BY msg_date";
+	mysql_query(&mysql, _q.c_str());
+	
+	if (res = mysql_store_result(&mysql)) {
+		while (row = mysql_fetch_row(res)) {
+			_s = row[0];
+			_r = row[1];
+			_t = row[2];
+			_m = row[3];
+			std::string msg = makeMsg("USTLST", _s, _r, _t, _m);
+
+			bzero(message, sizeof(message));
+			strcpy(message, msg.data());
+			send(index, message, sizeof(message), 0);
+						
+			recv(connection, message, MESSAGE_LENGTH, 0);
 		}
+		send(index, END, CODE_LENGTH, 0);
+		std::cout << "успешно" << std::endl;
 	}
 	else{
 		errorAccess(index);
@@ -322,6 +339,39 @@ void Server::optDialog(int index, std::string m){
 	}
 }
 
+void Server::userTyping(int index, std::string m){	
+	std::string from_temp, to_temp, _time, _msgbody, _q;
+	splitMsg(m, from_temp, to_temp, _time, _msgbody);
+	
+	_q = "INSERT INTO msgdata(id_sender, id_receiver, msg_date, msg_status, msg) values (" + from_temp + "," + to_temp + ",'" + _time + "',1,'" + _msgbody + "')";
+	mysql_query(&mysql, _q.c_str());
+
+	send(index, SUCCESS, CODE_LENGTH, 0);	
+}
+
+void Server::readUser(){
+	if(!_users.empty()){
+		_users.clear();
+	}
+
+	std::string _n, _id, _p;
+	mysql_query(&mysql, "SELECT userdata.id, u_name, u_pass FROM userhash join userdata on userhash.id = userdata.id");
+	if (res = mysql_store_result(&mysql)) {
+		while (row = mysql_fetch_row(res)) {
+			_id = row[0];
+			_n = row[1];
+			//_p = row[2]; // сохранение пароля в массив объектов
+
+			//User newUser(_id, _n, _p);
+			User newUser(_id, _n);
+			_users.push_back(newUser);
+			_usercount++;
+		}
+	}
+}
+
+/*
+// сохренение и загрузка из текстового файла 
 void Server::writeUser(){
 	fs::path filepath{"accounts.txt"};
 	std::ofstream file(filepath, std::ios::out);
@@ -352,6 +402,7 @@ void Server::readUser(){
 		file.close();
 	}
 }
+*/
 
 std::string Server::makeReq(std::string rtype, std::string v1, std::string v2){
 	std::string result;
@@ -376,18 +427,38 @@ void Server::splitReq(std::string &m, std::string &v1, std::string &v2){
 	v2 = temparr[2];
 }
 
-std::string Server::pkgUserList(){
+std::string Server::makeMsg(std::string rtype, std::string v1, std::string v2, std::string v3, std::string v4){
 	std::string result;
-	for(int i = 0; i < _users.size(); i++){
-		result.append(_users[i]._login);
+		result.append(rtype);
 		result.append(";");
-	}
+		result.append(v1); // отправитель
+		result.append(";");
+		result.append(v2); // получатель
+		result.append(";");
+		result.append(v3); // время
+		result.append(";");
+		result.append(v4); // сообщение
+		result.append(";");
 	return result;
+}
+
+void Server::splitMsg(std::string &m, std::string &v1, std::string &v2, std::string &v3, std::string &v4){
+	std::string temparr[5];
+	size_t pos{0};
+	int i{0};
+	while((pos = m.find(";")) != std::string::npos){
+		temparr[i++] = m.substr(0, pos); 
+		m.erase(0, pos + 1);
+	}
+	v1 = temparr[1];
+	v2 = temparr[2];
+	v3 = temparr[3];
+	v4 = temparr[4];
 }
 
 int Server::findLogin(std::string &user){
 	for(int i = 0; i < _usercount; i ++){
-		if(user == _users[i]._login){
+		if(user == _users[i]._name){
 			return i;
 		}
 	}
@@ -399,7 +470,7 @@ int Server::loginCheck(std::string &l){
 		if(l[i] == ';' || l [i] == '/' || l [i] == ':' ) { return 5; }
 	}
 	for(int i = 0; i < _users.size(); i++){
-		if(l == _users[i]._login) { return 4; }
+		if(l == _users[i]._name) { return 4; }
 	}
 	return 1;
 }
@@ -435,4 +506,57 @@ bool Server::User::getStatusAuth(){
 void Server::errorAccess(int &i){
 	send(i, ERROR_AUTH, CODE_LENGTH, 0);
 	std::cout << ERROR_MESSAGE << "Пользователь не авторизован!" << std::endl;
+}
+
+void Server::readConfig(){
+	std::ifstream cfg(config_path, std::ios::in);
+	if(cfg.is_open()){
+		std::string x, y;
+		while(getline(cfg, x, '\t')){
+			getline(cfg, y, '\n');
+				
+			if(x == "PORT"){ PORT = stoi(y); }
+			if(x == "sql_user"){ sql_user = y; }
+			if(x == "sql_pass"){ sql_pass = y; }
+			if(x == "sql_db"){ sql_db =  y; }
+		}	
+		cfg.close();
+	}
+	else{
+		PORT = 7777;
+		sql_user = "lightchat";
+		sql_pass = "lightchat"; 
+		sql_db = "lightchatdb";
+		writeConfig();
+	}
+}
+
+void Server::writeConfig(){
+	std::ofstream cfg(config_path, std::ios::out);
+    if(cfg.is_open()){
+		cfg << "PORT";
+		cfg << '\t';
+		cfg << PORT;
+		cfg << '\n';
+
+		cfg << "sql_user";
+		cfg << '\t';
+		cfg << sql_user;
+		cfg << '\n';
+
+		cfg << "sql_pass";
+		cfg << '\t';
+		cfg << sql_pass;
+		cfg << '\n';
+
+		cfg << "sql_db";
+		cfg << '\t';
+		cfg << sql_db;
+		cfg << '\n';
+
+		cfg.close();
+   } 	
+   else{
+		std::cout << "Ошибка открытия файла" << std::endl;
+   }
 }
--- serv/server.h
+++ serv/server.h
@@ -8,24 +8,32 @@
 class Server{
 public:
 	Server(){
+		readConfig();
+		sqlConn();
 		readUser();
 	};
 	~Server(){}
 	
 	void serverRuntime();
 	void serverStart();
+	void sqlConn();
+	void readConfig();
+	void writeConfig();
 	
 	void userReg(int index, std::string m);
 	void userAdd(std::string &l, std::string &p);
 	void userAuth(int index, std::string m);
 	void userLogout(int index, std::string m);	
-	void handShake(int index);
+//	void handShake(int index);
 	
 	void sendUserList(int index, std::string m);
+	void sendMsgList(int index, std::string m);
 	void optDialog(int index, std::string m);
+	void userTyping(int index, std::string m);	
 
 	void writeUser();
 	void readUser();
+	void readMsg();
 	void getUserList();
 
 	int findLogin(std::string &user);
@@ -34,6 +42,9 @@ public:
 	
 	std::string makeReq(std::string rtype, std::string v1, std::string v2);
 	void splitReq(std::string &m, std::string &v1, std::string &v2);
+	std::string makeMsg(std::string rtype, std::string v1, std::string v2, std::string v3, std::string v4);
+	void splitMsg(std::string &m, std::string &v1, std::string &v2, std::string &v3, std::string &v4);
+
 	std::string pkgUserList();
 	int loginCheck(std::string &l);
 
@@ -41,25 +52,33 @@ private:
 	enum userstatus { online, offline };
 	
 	struct User{
-		User() : _login(""), _pass(""), _status(userstatus::offline), _auth(false){}
-		User(std::string login, std::string pass) :
-					_login(login), _pass(pass), _status(userstatus::offline), _auth(false){}
+		User() : _name(""), _surname(""), _pass(""), _uid(0),_email("") ,_status(userstatus::offline), _auth(false){}
+		
+		User(std::string uid, std::string name) :
+					_name(name), _surname(""), _pass(""), _uid(uid), _email(""), _status(userstatus::offline), _auth(false){}
+		
+		//User(std::string uid, std::string uname, std::string pass) :
+		//			_name(uname), _surname(""), _pass(pass), _uid(uid), _email(""), _status(userstatus::offline), _auth(false){}
+		
 		~User(){}
 		
 		void switchStatus();
 		void switchStatusAuth();
 		bool getStatusAuth();
 		
-		std::string _login;
-		std::string _pass;
+		std::string _name; // обязательное поле, для входа в систему
+		std::string _surname;
+		std::string _pass; // обязательное поле
+		std::string _email;
+		std::string _uid; // обязательное поле, присваивается БД
+
 		userstatus _status;
 		bool _auth;
 	};
-/*	
+/*
 	struct Message{
 		Message() : _time(""), _sender(""), _receiver(""), _msg(""){}
-		Message(std::string msg) : _msg(msg){}
-		Message(std::string time, std::string _from, std::string _to, std::string msg) :
+		Message(std::string _from, std::string _to, std::string time, std::string msg) :
 					_time(time), _msg(msg), _sender(_from), _receiver(_to){}
 		~Message(){}
 		
@@ -69,13 +88,15 @@ private:
 		std::string _time;
 	};
 */
-	std::vector<User> _users{};
+	std::vector<User> _users{}; // для временного хранения авторизованных пользователей
 	//std::vector<Message> _messages{};
 	int _usercount{0};
+	int _msgcount{0};
 
-	int _sndr;
-	int _rcvr;
-
-	std::set<int> clients;
-	int _switcher{0};
+	// настраиваемые переменные
+	int PORT;
+	std::string sql_user;
+	std::string sql_pass; 
+	std::string sql_db;
+	std::string config_path = "config.ini";
 };
